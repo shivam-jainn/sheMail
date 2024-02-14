b7a919ccd1ec59b6dbd3434c4d070a8dd58da1df
shivam-jainn <shivam.clgstash@gmail.com>
2024-02-14 23:48:35 +0530

    added h1

--- emails/emails/SheCodes/SheCodesMail.tsx
+++ emails/emails/SheCodes/SheCodesMail.tsx
@@ -1,8 +1,9 @@
-import { Button, Html, Body, Tailwind } from "@react-email/components";
+import { Button, Html, Body, Tailwind, Heading } from "@react-email/components";
 import * as React from "react";
 import { Img } from "@react-email/components";
 
 export default function Email() {
+    const company = "Nom Nom"
   return (
     <Tailwind
     config={{
@@ -15,16 +16,12 @@ export default function Email() {
       },
     }}
   >
-    <Html className="flex flex-col items-center">
+    <Html className="font-mono flex flex-col items-center">
         <Body className="bg-white">
         <Img src={"../../static/SheCodesMailLogo.png"} />
         {/* <Img src="https://shemail-bucket-1.s3.ap-south-1.amazonaws.com/SheCodesMailLogo.jpg" /> */}
-      <Button
-        href="https://example.com"
-        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
-      >
-        Click me
-      </Button>
+
+        <Heading as="h2">Hello , {company}</Heading>
       </Body>
     </Html>
     </Tailwind>
