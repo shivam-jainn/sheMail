42dd96b2b97d9810b3ac4e6605d9085da1d4d81d
shivam-jainn <shivam.clgstash@gmail.com>
2024-02-14 23:46:34 +0530

    added tailwind

--- emails/emails/SheCodes/SheCodesMail.tsx
+++ emails/emails/SheCodes/SheCodesMail.tsx
@@ -1,17 +1,32 @@
-import { Button, Html } from "@react-email/components";
+import { Button, Html, Body, Tailwind } from "@react-email/components";
 import * as React from "react";
 import { Img } from "@react-email/components";
 
 export default function Email() {
   return (
+    <Tailwind
+    config={{
+      theme: {
+        extend: {
+          colors: {
+            brand: "#007291",
+          },
+        },
+      },
+    }}
+  >
     <Html className="flex flex-col items-center">
-        <Img src="https://shemail-bucket-1.s3.ap-south-1.amazonaws.com/SheCodesMailLogo.jpg" />
+        <Body className="bg-white">
+        <Img src={"../../static/SheCodesMailLogo.png"} />
+        {/* <Img src="https://shemail-bucket-1.s3.ap-south-1.amazonaws.com/SheCodesMailLogo.jpg" /> */}
       <Button
         href="https://example.com"
         style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
       >
         Click me
       </Button>
+      </Body>
     </Html>
+    </Tailwind>
   );
 }
